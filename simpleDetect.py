e3831e96e93aaf1e28797ccca8d31b6198fb8a51
zuoruihai <zuoruihai@58.com>
2022-08-10 19:52:04 +0800

    新增

--- simpleDetect.py
+++ simpleDetect.py
@@ -5,9 +5,10 @@ import numpy as np
 import torch
 from models.experimental import attempt_load
 from utils.general import non_max_suppression, scale_coords
+from utils.torch_utils import time_sync
 
 if __name__ == "__main__":
-    start_time = time.time()
+    start_time = time_sync()
 
     use_gpu = torch.cuda.is_available()
 
@@ -21,7 +22,7 @@ if __name__ == "__main__":
 
     height, width = 640, 640
 
-    cost = time.time() - start_time
+    cost = time_sync() - start_time
     print(cost)
 
     img0 = cv2.imread('data/images/bus.jpg')
@@ -39,13 +40,15 @@ if __name__ == "__main__":
         pred = pred.cpu()
     pred.clone().detach()
     pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)  # 非极大值抑制
+    names = model.names
     for i, det in enumerate(pred):
         if len(det):
             det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
             for *xyxy, conf, cls in reversed(det):
-                print('{},{},{}'.format(xyxy, conf.numpy(), cls.numpy()))  # 输出结果：xyxy检测框左上角和右下角坐标，conf置信度，cls分类结果
+                print('{},{},{},{}'.format(xyxy, conf.numpy(), cls.numpy(),
+                                           names[int(cls)]))  # 输出结果：xyxy检测框左上角和右下角坐标，conf置信度，cls分类结果
                 img0 = cv2.rectangle(img0, (int(xyxy[0].numpy()), int(xyxy[1].numpy())),
                                      (int(xyxy[2].numpy()), int(xyxy[3].numpy())), (0, 255, 0), 2)
-    cost = time.time() - start_time
+    cost = time_sync() - start_time
     print(cost)
     cv2.imwrite('out.jpg', img0)  # 简单画个框
